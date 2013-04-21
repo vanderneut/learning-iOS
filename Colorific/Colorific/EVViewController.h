92cc707a00f165dd35f1578e7a31860ce8f8a3fb
vanderneut <vanderneut@gmail.com>
2013-04-20 22:39:41 -0400

    Can now touch the screen to change the color randomly.

--- Colorific/Colorific/EVViewController.h
+++ Colorific/Colorific/EVViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface EVViewController : UIViewController
-
+- (IBAction)changeColor:(id)sender;
 @end
--- Colorific/Colorific/EVViewController.m
+++ Colorific/Colorific/EVViewController.m
@@ -26,4 +26,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeColor:(id)sender
+{
+    int r = arc4random() % 255;     // random red
+    int g = arc4random() % 255;     // random green
+    int b = arc4random() % 255;     // random blue
+    
+    UIColor *color = [UIColor colorWithRed:(r / 255.0) green:(g / 255.0) blue:(b / 255.0) alpha:1.0];
+    [self.view setBackgroundColor:color];
+}
 @end
