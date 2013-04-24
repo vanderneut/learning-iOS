77317d43261a9e16e3e6dc5af234f62fdd7a5fa5
vanderneut <vanderneut@gmail.com>
2013-04-24 04:18:49 -0400

    MyFirstSingleViewApp
    Apparently these files weren't staged and committed yet?  ** confused **  Found out when trying Tower instead of SourceTree.

---
+++ tutsplus.com/MyFirstSingleViewApp/MyFirstSingleViewApp/EVViewController.h
@@ -0,0 +1,13 @@
+//
+//  EVViewController.h
+//  MyFirstSingleViewApp
+//
+//  Created by Erik van der Neut on 4/20/13.
+//  Copyright (c) 2013 Erik van der Neut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EVViewController : UIViewController
+
+@end
---
+++ tutsplus.com/MyFirstSingleViewApp/MyFirstSingleViewAppTests/MyFirstSingleViewAppTests.m
@@ -0,0 +1,32 @@
+//
+//  MyFirstSingleViewAppTests.m
+//  MyFirstSingleViewAppTests
+//
+//  Created by Erik van der Neut on 4/20/13.
+//  Copyright (c) 2013 Erik van der Neut. All rights reserved.
+//
+
+#import "MyFirstSingleViewAppTests.h"
+
+@implementation MyFirstSingleViewAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MyFirstSingleViewAppTests");
+}
+
+@end
